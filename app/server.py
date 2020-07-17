44967d81b7b77487df73284ba69803756eecfdff
Shama Barna, PhD <45246980+shamafarabi@users.noreply.github.com>
2020-07-17 13:02:40 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,12 +9,13 @@ from fastai import *
 # from fastai.vision import *
 from fastai.text import *
 
-export_file_url =  'https://drive.google.com/uc?export=download&id=1Zbtqw3pUCyOhReSPQDvX6jMcK9ax1k_P'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Zbtqw3pUCyOhReSPQDvX6jMcK9ax1k_P'
 
 # export_file_url = 'https://www.dropbox.com/s/xhnvw0axn6xjbk9/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 # added comment
-classes = ['neg', 'pos']
+classes=['CHANDLER', 'JOEY', 'MONICA', 'PHOEBE', 'RACHEL', 'ROSS']
+#classes = ['neg', 'pos']
 path = Path(__file__).parent
 
 app = Starlette()
